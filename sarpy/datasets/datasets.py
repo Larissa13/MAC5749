3e696f4b883b57ca45cd954198e31e3b41862bac
Marcos Tejada <marcostejadac@gmail.com>
2017-08-25 19:10:07 -0300

    update nist

--- sarpy/datasets/datasets.py
+++ sarpy/datasets/datasets.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 from skimage import io
 import pandas as pd
+import binascii
 
 # Root directory for this package
 root_directory = os.path.dirname(__file__)
@@ -64,8 +65,16 @@ def load_mnist():
 
     return {'bitmaps': X, 'targets': y}
 
-def load_nist():
+def load_nist(block=0):
     """Loads the NIST character dataset. [add more info]
+    Parameters
+    ----------
+        block : int
+            size of database loaded, 
+                0 - "100 images by class"       -> total 6200
+                1 - "1000 images by class"      -> total 62000
+                2 - "max 10000 images by class" -> total 397621 images
+
     Returns
     -------
     dataset : dict
@@ -73,5 +82,40 @@ def load_nist():
         a vector of the images of the dataset as 2D NumPy arrays,
         and 'targets', a vector of the target/class of each image.
     """
+    dataset_directory = os.path.join(root_directory,'NIST19')    
+    if(block == 2):
+        bitmaps = np.load(dataset_directory+'/train_nist19_bitmaps_lim10000.npz')['bitmaps']
+        targets = np.load(dataset_directory+'/train_nist19_targets_lim10000.npz')['targets']
+        names   = np.load(dataset_directory+'/train_nist19_names_lim10000.npz')['names']        
+    else:
+        if(block == 1):
+            bitmaps = np.load(dataset_directory+'/train_nist19_bitmaps_1000.npz')['bitmaps']
+            targets = np.load(dataset_directory+'/train_nist19_targets_1000.npz')['targets']
+            names   = np.load(dataset_directory+'/train_nist19_names_1000.npz')['names']
+        else:
+            directory_class = os.path.join(dataset_directory,'by_class')    
+            list_of_class = os.listdir(directory_class)
+            list_of_class.sort()
+            bitmaps = [];
+            targets = [];
+            names   = [];
+            id_class = 0;
 
-    return {'bitmaps': None, 'targets': None}
+            for a_class in list_of_class:                
+                    directory_subclass = os.path.join(directory_class,a_class)
+                    list_of_subclass = [fn for fn in os.listdir(directory_subclass) if ("train" in fn)]
+                    sorted(list_of_subclass)
+                    for folder_of_images in list_of_subclass:
+                            directory_of_images = os.path.join(directory_subclass,folder_of_images)
+                            list_of_images = os.listdir(directory_of_images)
+                            sorted(list_of_images)
+                            for filename in list_of_images:
+                                    img = np.where(io.imread(os.path.join(directory_of_images,filename)) > 0, 0, 1)                                    
+                                    bitmaps.append(img.astype(np.int8))
+                                    targets.append(id_class);
+                    id_class += 1
+                    names.append(binascii.unhexlify(a_class).decode('UTF-8'));        
+            
+    
+    return {'bitmaps': bitmaps, 'targets': targets, 'names':names}
+
