0f3508498caf7da2480f832b08523068e63601d2
Marcos Tejada <marcostejadac@gmail.com>
2017-09-10 22:31:19 -0300

    add function contour2bm

--- sarpy/binaryScale.py
+++ sarpy/binaryScale.py
@@ -1,6 +1,19 @@
 import numpy as np
 from math import ceil
 
+# shapeScale(img, c, center=(0,0))
+# Scale image by a certain factor.
+# Parameters:
+# * img: ndarray
+#   - Input binary image
+# * c: {float, tuple of floats}
+#    - Scale factors. Separate factors can be defined as (row_scale, col_scale)
+# * center: tuple of ints, optional
+#    - (x,y)-coordinates of the center of the image
+# Returns:
+# * g: ndarray
+#     - Scaled version of the input image
+
 def shapeScale(img, c, center=(0,0)):
     
     if type(c) == type((0,0)):
@@ -22,3 +35,22 @@ def shapeScale(img, c, center=(0,0)):
                 else:
                     g[i,j] = 0
     return g
+
+def shapeScale2(img, c):
+    if type(c) == type((0,0)):
+        h2 = floor(c[0])
+        w2 = floor(c[1])
+    else:
+        raise ValueError('Error in arguments')
+        
+    h1,w1=img.shape
+    x_ratio = w1/w2;
+    y_ratio = h1/h2;    
+    g = np.zeros(h2*w2)
+    for i in range(h2):
+        for j in range(w2):
+            px = floor(j*x_ratio) ;
+            py = floor(i*y_ratio) ;
+            g[(i*w2)+j] = img[py][px];
+    g = g.reshape(h2,w2)
+    return g;
--- sarpy/plotContour.py
+++ sarpy/plotContour.py
@@ -5,19 +5,37 @@ def plotContour(c):
     
     if (type(c) == type(np.array(()))) & (c.shape[1] >= 3):
             
-        x = np.amax(c[:,1])+1
-        y = np.amax(c[:,2])+1
-        img = np.zeros((x,y), dtype='int16')
+        w = np.amax(c[:,1])+1
+        h = np.amax(c[:,2])+1
+        img = np.zeros((h,w), dtype='int8')
         
         for i in range(c.shape[0]):
             a = c[i,1]
             b = c[i,2]
-            img[a,b] = 1
+            img[b,a] = 1
             
         fig, ax = plt.subplots()
-        ax.imshow(img)
+        ax.imshow(img,cmap='gray')
         ax.axis('off')
         plt.show()
         
     else:
         raise ValueError('Error: the argument has to be a ndarray with at least 3 columns')
+
+def contour2bm(c):
+    
+    if (type(c) == type(np.array(()))) & (c.shape[1] >= 3):
+            
+        w = np.amax(c[:,1])+1  # replace with img width 
+        h = np.amax(c[:,2])+1  # replace with img height
+        img = np.zeros((h,w), dtype='int8')
+        
+        for i in range(c.shape[0]):
+            x = c[i,1]
+            y = c[i,2]
+            img[y,x] = 1
+            
+        return img;       
+
+    else:
+        raise ValueError('Error: the argument has to be a contour')        
