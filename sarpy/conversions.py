7e18b612856c395376921ce0fdf4453c5cb5b33d
MarEe0 <mar.ee.riva@gmail.com>
2017-09-13 12:46:18 -0300

    Added some conversions to conversions.py

--- sarpy/conversions.py
+++ sarpy/conversions.py
@@ -2,9 +2,11 @@
 Methods for converting shape representations into other shape representations.
 """
 
+from skimage import measure
+
 from sarpy.utils import *
 
-def convert_shape(shape, new_type):
+def convert_shape(shape, new_type, **kwargs):
     """
         Converts a given shape into another representation.
 
@@ -13,6 +15,8 @@ def convert_shape(shape, new_type):
           - Input shape
         * new_type: int
           - New type of the shape. Must be one of the types defined in sarpy.utils.
+        * kwargs
+          - Arguments for the different conversions.
         Returns:
         * converted_shape: Shape
             - Converted representation of the input shape
@@ -42,9 +46,74 @@ def convert_shape(shape, new_type):
 def convert_to_bitmap(shape):
     pass
 def convert_to_contour(shape):
-    pass
+    """
+    Converts a given shape into a contour.
+    Parameters:
+        * shape: Shape
+          - Input shape
+        Returns:
+        * converted_shape: Shape
+            - Contour representation of the input shape
+    """
+
+    if shape.shape_type == type_contour: # New type is the same as current type, i.e.: no conversion
+        return shape
+
+    if shape.shape_type == type_bitmap: # Bitmap to contour
+        contours = measure.find_contours(img, 0)
+        contours_lens = np.array([len(c) for c in contours])
+        sort_order = (-contours_lens).argsort()
+        return Shape(type_contour, [[np.array([j, contours[i][j][0], contours[i][j][1]], dtype = int) for j in range(len(contours[i]))] for i in sort_order])
+
+    if new_type == type_point_set: # Point set to contour
+        pass
+    if new_type == type_complex_contour: # Complex contour to contour
+        pass
+    if new_type == type_landmark: # Landmark to contour
+        pass
+    if new_type == type_polar_contour: # Polar contour to contour
+        pass
+
+
 def convert_to_point_set(shape):
-    pass
+    """
+    Converts a given shape into a pointset.
+    Parameters:
+        * shape: Shape
+          - Input shape
+        Returns:
+        * converted_shape: Shape
+            - Point set representation of the input shape
+    """
+
+    if shape.shape_type == type_point_set: # New type is the same as current type, i.e.: no conversion
+        return shape
+
+    if shape.shape_type == type_bitmap: # Bitmap to point set
+        bitmapImage = shape.data
+        pointSet = []
+        row = 0
+        while row < bitmapImage.shape[0]:
+            column = 0
+            while column + 1 < bitmapImage.shape[1]:
+                if (bitmapImage[row][column] != bitmapImage[row][column + 1]):
+                     if ((not bitmapImage[row][column]) and (bitmapImage[row][column + 1])) or ((bitmapImage[row][column]) and not (bitmapImage[row][column + 1])):
+                        pointSet.append([row + 1, column + 1])
+                column += 1
+            row += 1
+        
+        return Shape(type_point_set, pointSet)
+
+    if new_type == type_contour: # Contour to point set
+        pass
+    if new_type == type_complex_contour: # Complex contour to point set
+        pass
+    if new_type == type_landmark: # Landmark to point set
+        pass
+    if new_type == type_polar_contour: # Polar contour to point set
+        pass
+
+
 def convert_to_complex_contour(shape):
     pass
 def convert_to_landmark(shape):
