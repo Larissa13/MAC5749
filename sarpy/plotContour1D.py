c9bf4bc27c344b15a42f087f139fc62d837c12e0
marivgonzales <marivgonzales@gmail.com>
2017-09-15 11:12:15 -0300

    Update plotContour1D.py

--- sarpy/plotContour1D.py
+++ sarpy/plotContour1D.py
@@ -1,9 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plotContour1D(c):
+def plotContour1D(c, show='default'):
     
     if (type(c) == type(np.array(()))) & (c.shape[1] >= 3):
+        
+        if (show == 'default'):
+        
+            fig1 = plt.figure()
+            ax1 = fig1.add_subplot(111)
+            ax1.plot(c[:,0], c[:,1], color='blue', marker='*')
+            ax1.set(ylabel='Contour Coordinate', xlabel='Position')
+            ax1.legend(['line index'])
+            
+            fig2 = plt.figure()
+            ax2 = fig2.add_subplot(111)
+            ax2.plot(c[:,0], c[:,2], color='red', marker='o')
+            ax2.set(ylabel='Contour Coordinate', xlabel='Position')
+            ax2.legend(['column index'])
+            
+            plt.show()
+            
+        elif (show == 'together'):
         
             fig = plt.figure()
             ax = fig.add_subplot(111)
@@ -11,7 +29,31 @@ def plotContour1D(c):
             ax.plot(c[:,0], c[:,2], color='red', marker='o')
             ax.set(ylabel='Contour Coordinate', xlabel='Position')
             ax.legend(['line index', 'column index'])
+            
             plt.show()
             
+        elif (show == 'line'):
+        
+            fig1 = plt.figure()
+            ax1 = fig1.add_subplot(111)
+            ax1.plot(c[:,0], c[:,1], color='blue', marker='*')
+            ax1.set(ylabel='Contour Coordinate', xlabel='Position')
+            ax1.legend(['line index'])
+            
+            plt.show()
+            
+        elif (show == 'column'):
+            
+            fig2 = plt.figure()
+            ax2 = fig2.add_subplot(111)
+            ax2.plot(c[:,0], c[:,2], color='red', marker='o')
+            ax2.set(ylabel='Contour Coordinate', xlabel='Position')
+            ax2.legend(['column index'])
+            
+            plt.show()
+            
+        else:
+            raise ValueError('Error: the argument show has to be "default", "together", "line" or "column"')
+            
     else:
-        raise ValueError('Error: the argument has to be a ndarray with at least 3 columns')
+        raise ValueError('Error: the argument c has to be a ndarray with at least 3 columns')
