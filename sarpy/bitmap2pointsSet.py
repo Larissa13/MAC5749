4188cec3f2626e930cd467ee1458978ab9fa3b6b
ademirtc <marcostejadac@gmail.com>
2017-09-17 17:52:33 -0300

    mod function bitmap2setpoint

--- sarpy/bitmap2pointsSet.py
+++ sarpy/bitmap2pointsSet.py
@@ -1,13 +1,23 @@
-def bitmap2pointsSet(bitmapImage):
-    pointSet = []
-    row = 0
-    while row < bitmapImage.shape[0]:
-        column = 0
-        while column + 1 < bitmapImage.shape[1]:
-            if (bitmapImage[row][column] != bitmapImage[row][column + 1]):
-                 if ((not bitmapImage[row][column]) and (bitmapImage[row][column + 1])) or ((bitmapImage[row][column]) and not (bitmapImage[row][column + 1])):
-                    pointSet.append([row + 1, column + 1])
-            column += 1
-        row += 1
-    
-    return (pointSet)
+def bitmap2setPoints(bitmap,adj=4):
+    adjx = []
+    adjy = []
+    if adj == 8:
+        adjx = [-1,0,1,-1,1,-1,0,1];
+        adjy = [-1,-1,-1,0,0,1,1,1];
+    else:
+        adjx = [0,-1,1,0];
+        adjy = [-1,0,0,1];
+        
+    adjn = len(adjx)
+    nrows,ncols = bitmap.shape
+    setPoints = set();
+    for i in range(0,nrows):
+        for j in range(0,ncols):
+            if bitmap[i,j] == 1:
+                for dx,dy in zip(adjx,adjy):        
+                    x = i + dx
+                    y = j + dy
+                    if x >= 0 and x < ncols and y >=0 and y < nrows:
+                        if bitmap[x,y] == 0:                        
+                            setPoints.add(tuple([x,y]))
+    return list(setPoints)
