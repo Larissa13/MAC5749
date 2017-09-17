6ed0d1354fe4776dfe17490202b527f2a1e14dde
ademirtc <marcostejadac@gmail.com>
2017-09-17 17:00:24 -0300

    mod theme docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,8 +95,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'classic'
-# html_theme = 'nature'
+# html_theme = 'classic'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
